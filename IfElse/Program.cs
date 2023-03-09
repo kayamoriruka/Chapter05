a68ea40e341791c7cd087dac87d01ac486968e21
kayamoriruka <kayamoriruka@hotmail.com>
2023-03-10 08:58:00 +0900

    cs

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IfElse
 {
@@ -11,6 +7,7 @@ namespace IfElse
         static void Main(string[] args)
             
         {
+            //
         }
     }
 }
