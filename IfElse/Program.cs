09ea5517bd55fd303959e4ac6035c9f33c81a587
kayamoriruka <kayamoriruka@hotmail.com>
2023-03-10 15:29:12 +0900

    0310

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -7,7 +7,29 @@ namespace IfElse
         static void Main(string[] args)
             
         {
-            //
+            Console.Write("숫자를 입력하세요: ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            Console.WriteLine($"{number}");
+
+            if (number < 0)
+                Console.WriteLine("음수");
+            else if (number > 0)
+                Console.WriteLine("양수");
+            else
+                Console.WriteLine("0");
+
+            if (number % 2 == 0)
+                Console.WriteLine("짝수");
+            else
+                Console.WriteLine("홀수");
+
+            //string result = number % 2 == 0 ? "짝수" : "홀수";
+            //Console.WriteLine(result);
+
+
         }
     }
 }
